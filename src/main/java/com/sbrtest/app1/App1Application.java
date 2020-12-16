379a8d437f813b6d47383596147daf63bb5b0bf6
romashovd <46222858+romashovd@users.noreply.github.com>
2020-12-16 03:36:29 +0300

    +

--- src/main/java/com/sbrtest/app1/App1Application.java
+++ src/main/java/com/sbrtest/app1/App1Application.java
@@ -1,12 +1,19 @@
 package com.sbrtest.app1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan
 public class App1Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
+
         SpringApplication.run(App1Application.class, args);
     }
 
